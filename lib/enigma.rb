f9c5c7af40d71f293cf228c62803734f464706fc
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-12 11:39:02 -0800

    Add feature decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,8 +5,8 @@ class Enigma
 
   def encrypt(msg, key_num, date)
     @e = { encryption: '',
-          key: key_num,
-          date: date }
+           key: key_num,
+           date: date }
     key = Key.new(key_num)
     offset = Offset.new(date)
     shift = Shift.new(key, offset)
@@ -36,6 +36,25 @@ class Enigma
       shift.c_shift
     else
       shift.d_shift
+    end
+  end
+
+  def decrypt(msg, key_num, date)
+    @d = { decryption: '',
+           key: key_num,
+           date: date }
+           key = Key.new(key_num)
+    offset = Offset.new(date)
+    shift = Shift.new(key, offset)
+    dnew_char(msg, shift)
+    @d
+  end
+
+  def dnew_char(msg, shift)
+    msg.each_char.with_index do |char, index|
+      new_index = characters.index(char) - assoc_shift(index, shift)
+      new_index += 27 while new_index < 0
+      @d[:decryption] << characters[new_index]
     end
   end
 end
