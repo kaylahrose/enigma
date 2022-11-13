d1190ad23a5071104dfbf2c279f2460517504ebd
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-12 20:43:51 -0800

    Refactor

---
+++ lib/cipher.rb
@@ -0,0 +1,39 @@
+module Cipher
+  def cipher(msg)
+    msg.each_char.with_index do |char, index|
+      new_char(char, index)
+    end
+    @hash
+  end
+
+  def new_char(char, index)
+    if characters.index(char)
+      shifted_characters = shift(index)
+      char = shifted_characters[characters.index(char)]
+    end
+    @hash[@hash.keys[0]] << char
+  end
+
+  def shift(index)
+    shift = make_shifts(@hash[:key], @hash[:date])
+    shift = if @hash[:encryption]
+              assoc_shift(index, shift)
+            else
+              -assoc_shift(index, shift)
+            end
+    characters.rotate(shift)
+  end
+
+  def assoc_shift(index, shift)
+    index -= 4 while index > 3
+    if index == 0
+      shift.a_shift
+    elsif index == 1
+      shift.b_shift
+    elsif index == 2
+      shift.c_shift
+    else
+      shift.d_shift
+    end
+  end
+end
