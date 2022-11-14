063cd066cbdd68fdabde8a25d4a91c14a18b4f58
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-13 16:43:45 -0800

    Refactor shift

--- lib/shift.rb
+++ lib/shift.rb
@@ -7,4 +7,18 @@ class Shift
     @c_shift = key.c_key + offset.c_offset
     @d_shift = key.d_key + offset.d_offset
   end
+
+  def assoc_shift(index)
+    index -= 4 while index > 3
+    case index
+    when 0
+      a_shift
+    when 1
+      b_shift
+    when 2
+      c_shift
+    when 3
+      d_shift
+    end
+  end
 end
